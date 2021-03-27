483b88333e8ae2e10b6a8d25e5f7f9572aa5b530
evyang <evyang@cs.washington.edu>
2021-03-27 13:22:54 -0700

    Updated route definitions.

--- src/components/GameArea.js
+++ src/components/GameArea.js
@@ -9,15 +9,14 @@ const GameArea = ({snake, food, move, gameOver, reset}) => {
     return (
         <Router>
         <div className="game-area">
-            <Route path="/" exact render={() => (<Home/>)}/>
-            <Route path="/play" exact />
-            <Route path="/instructions" exact component={Instructions}/>
-            <Route path="/play" exact>
-                {gameOver ? <Redirect to="/end"/> : <Play snake={snake} food={food} move={move}/>}
+            <Route path={process.env.PUBLIC_URL + "/"} exact render={() => (<Home/>)}/>
+            <Route path={process.env.PUBLIC_URL + "/instructions"} exact component={Instructions}/>
+            <Route path={process.env.PUBLIC_URL + "/play"} exact>
+                {gameOver ? <Redirect to={process.env.PUBLIC_URL + "/end"}/> : <Play snake={snake} food={food} move={move}/>}
             </Route>
-            <Route path="/end" exact render={() => (<EndGame score={snake.length - 3} reset={reset}/>)}/>
+            <Route path={process.env.PUBLIC_URL + "/end"} exact render={() => (<EndGame score={snake.length - 3} reset={reset}/>)}/>
         </div>
-        <Route path="/play" exact render={() => (<Score score={snake.length - 3}/>)}/>
+        <Route path={process.env.PUBLIC_URL + "/play"} exact render={() => (<Score score={snake.length - 3}/>)}/>
         </Router>
     );
 }
